46b1279e32391a8df1f91f6862b08e188f0fbf66
lynnie07 <dcao389@students.fairmontschools.com>
2024-10-22 13:23:32 -0700

    completed

--- Main.java
+++ Main.java
@@ -1,55 +1,42 @@
-import java.util.Scanner;
 import java.util.Random;
-    
-    public class Main{ 
-    public static void main(String[]args){
+import java.util.Scanner;
+
+public class Main { 
+    public static void main(String[] args) {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
 
-        int x = rand.nextInt(bound:100);
+        int x = rand.nextInt(100);  // Corrected the syntax
         int guess;
-        System.out.println(x:"Enter your first guess:");
+        System.out.println("Enter your first guess:"); // Corrected the syntax
         guess = scan.nextInt();
-        int count=0; //count the number of times user guesses
+        int count = 0; // Count the number of times user guesses
 
-        while(guess!=x){
-            //if x less than, greater than or equal
-            if (count <=7);
-                if (guess < x){
-                System.out.print("Too Low!");
-                }
-                count+=1;
-
-                if (guess == x ){
-                System.out.print("You got an impossibly good score!");
-                }
-
-                else{
-                System.out.print("Too High!");
+        while (guess != x && count < 10) {  // Added condition to prevent infinite loop
+            if (guess < x) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Too High!");
             }
-                count+=1;
 
-             if (count<=10){
-                    if (guess < x){
-                        System.out.print("Too Low!");
-                        }
-                        count+=1;
-        
-                    if (guess == x ){
-                        System.out.print("You did a good job!");
-                    }
-
-             else{
-            
-                    System.out.print("Try the divide and conquer strategy next time!");
-                    }
-    
+            count++;
+            if (count < 10) {
+                System.out.println("Try again:");
+                guess = scan.nextInt();
             }
-            
-                    
-            }
-        
-
         }
-        
+
+        // Check if user guessed correctly
+        if (guess == x) {
+            if (count <= 7) {
+                System.out.println("You got an impossibly good score!");
+            } else if (count <= 10) {
+                System.out.println("You did a good job!");
+            }
+        } else {
+            System.out.println("Try the divide and conquer strategy next time!");
+        }
+
+        scan.close(); // Close the scanner to avoid resource leaks
     }
+}
